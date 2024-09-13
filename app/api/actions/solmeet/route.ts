0b2e6c4f2dacd62304804761ed9327a2d6350566
thrishank <thrishankkalluru@gmail.com>
2024-09-13 18:29:04 +0530

    add get slots route

--- app/api/actions/solmeet/route.ts
+++ app/api/actions/solmeet/route.ts
@@ -1,17 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 import {
-    ActionError,
-    ActionGetResponse,
-    ActionPostRequest,
-    ActionPostResponse,
-    createActionHeaders,
-    createPostResponse,
-    NextActionLink,
-  } from "@solana/actions";
-  import { PublicKey } from "@solana/web3.js";
-  
-  const headers = createActionHeaders();
-  const prisma=new PrismaClient();
+  ActionError,
+  ActionGetResponse,
+  ActionPostRequest,
+  ActionPostResponse,
+  createActionHeaders,
+  createPostResponse,
+  NextActionLink,
+} from "@solana/actions";
+import { PublicKey } from "@solana/web3.js";
+import axios from "axios";
+
+const headers = createActionHeaders();
+const prisma = new PrismaClient();
 
 // async function data(username:string,slug:string){
 //   const res = await fetch(`https://cal.com/api/trpc/public/slots.getSchedule?input=%7B%22json%22%3A%7B%22isTeamEvent%22%3Afalse%2C%22usernameList%22%3A%5B%22${username}%22%5D%2C%22eventTypeSlug%22%3A%22${slug}%22%2C%22startTime%22%3A%222024-08-31T18%3A30%3A00.000Z%22%2C%22endTime%22%3A%222024-09-30T18%3A29%3A59.999Z%22%2C%22timeZone%22%3A%22Asia%2FCalcutta%22%2C%22duration%22%3Anull%2C%22rescheduleUid%22%3Anull%2C%22orgSlug%22%3Anull%2C%22teamMemberEmail%22%3Anull%7D%2C%22meta%22%3A%7B%22values%22%3A%7B%22duration%22%3A%5B%22undefined%22%5D%2C%22orgSlug%22%3A%5B%22undefined%22%5D%2C%22teamMemberEmail%22%3A%5B%22undefined%22%5D%7D%7D%7D`, {
@@ -37,183 +38,113 @@ import {
 //   console.log(data1.result.data.json.slots);
 // }
 
+export async function GET(req: Request) {
+  const url = new URL(req.url);
+  const id = url.searchParams.get("id");
+  const data = await prisma.solMeet.findUnique({
+    where: {
+      id: id!,
+    },
+  });
 
-  export async function GET(req:Request) {
-    const url=new URL(req.url);
-    const id=url.searchParams.get("id");
-    const data=await prisma.solMeet.findUnique({
-        where:{
-            id:id!,
-        }
-    })
-    try {
-      const payload: ActionGetResponse = {
-        title: `${data!.title}`,
-        description: `${data!.description}`,
-        label: "Connect",
-        icon: `${data!.image}`,
-        type: "action",
-        links: {
-          actions: [
-            {
-              label: "Connect your wallet",
-              href: "/api/actions/solmeet/create",
-              parameters: [
-                {
-                  type: "text",
-                  label: "Enter your cal.com username",
-                  name: "username",
-                  required: true,
-                },
-                {
-                  type:"select",
-                  options:[
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    },
-                    {
-                      label:"afsd",
-                      value:"fs"
-                    }
-                    
+  let slotdata = {
+    json: {
+      usernameList: ["thrishank"],
+      eventTypeSlug: "15min",
+      startTime: "2024-09-13T18:30:00.000Z",
+      endTime: "2024-10-30T18:29:59.999Z",
+      timeZone: "Asia/Calcutta",
+    },
+  };
+  const cal_url =
+    "https://cal.com/api/trpc/public/slots.getSchedule?input=" +
+    encodeURIComponent(JSON.stringify(slotdata));
 
-                  ],
-                  name:"fsdaff"
-                }
-              ],
-            },
-          ],
-        },
-      };
-  
-      return Response.json(payload, { headers });
-    } catch (err) {
-      console.log(err);
-      let actionError: ActionError = { message: "An unknown error occurred" };
-      return Response.json(actionError, {
-        status: 400,
-        headers,
-      });
-    }
+  const res = await axios.get(cal_url);
+  let slotData = res.data.result.data.json.slots;
+  let slotObjects = createSlotObjects(slotData);
+  console.log(slotObjects.length);
+  try {
+    const payload: ActionGetResponse = {
+      title: `${data!.title}`,
+      description: `${data!.description}`,
+      label: "Connect",
+      icon: `${data!.image}`,
+      type: "action",
+      links: {
+        actions: [
+          {
+            label: "labe 2 your wallet",
+            href: "/api/actions/solmeet/create",
+          },
+          {
+            label: "Connect your wallet",
+            href: "/api/actions/solmeet/create",
+            parameters: slotObjects,
+          },
+        ],
+      },
+    };
+    return Response.json(payload, { headers });
+  } catch (err) {
+    console.log(err);
+    let actionError: ActionError = { message: "An unknown error occurred" };
+    return Response.json(actionError, {
+      status: 400,
+      headers,
+    });
   }
+}
+
+async function getCokkie(username: string) {
+  const res = await axios.get("https://cal.com/api/auth/session", {
+    headers: {
+      Referer: `https://cal.com/${username}`,
+    },
+  });
+  // @ts-ignore
+  return res.headers.get("set-cookie");
+}
+
+interface Slot {
+  time: string;
+}
+
+interface SlotData {
+  [date: string]: Slot[];
+}
+
+interface Option {
+  label: string;
+  value: string;
+}
+
+interface ActionParameterSelectable<Type extends string> {
+  type: Type; // Type is constrained to specific values like "select"
+  label: string;
+  options: Option[];
+  name: string;
+}
+
+// Define the specific type for 'select'
+type SelectActionParameter = ActionParameterSelectable<"select">;
+
+const createSlotObjects = (slots: SlotData): SelectActionParameter[] => {
+  return Object.entries(slots).reduce<SelectActionParameter[]>(
+    (acc, [date, times]) => {
+      if (times.length > 0) {
+        acc.push({
+          type: "select",
+          label: `Select a slot for ${date}`,
+          options: times.map((slot) => ({
+            label: new Date(slot.time).toLocaleTimeString(),
+            value: slot.time,
+          })),
+          name: date,
+        });
+      }
+      return acc;
+    },
+    []
+  );
+};
